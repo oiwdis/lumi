e44307e4644bab913f015fea4516b7e7f7eb8438
Elliot Ma <elliot@themaclan.com>
2026-06-26 10:07:22 -0700

    fix: Express 5 wildcard route syntax

--- server/index.js
+++ server/index.js
@@ -102,7 +102,7 @@ app.post('/api/tutor', async (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   const distPath = join(__dirname, '../dist');
   app.use(express.static(distPath));
-  app.get('*', (req, res) => res.sendFile(join(distPath, 'index.html')));
+  app.get('/{*path}', (req, res) => res.sendFile(join(distPath, 'index.html')));
 }
 
 const PORT = process.env.PORT || 3001;
